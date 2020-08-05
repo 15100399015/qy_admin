0d29e253835efd509d385637e1b0cc8b06605b9d
1474862431@qq.com <1474862431@qq.com>
2020-08-05 17:44:30 +0800

    master

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,7 +6,6 @@
       :model="loginForm"
       :rules="loginRules"
       class="login-form"
-      autocomplete="on"
       label-position="left"
     >
       <div class="title-container">
@@ -24,33 +23,23 @@
           name="username"
           type="text"
           tabindex="1"
-          autocomplete="on"
         />
       </el-form-item>
 
-      <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
-        <el-form-item prop="password">
-          <span class="svg-container">
-            <svg-icon icon-class="password" />
-          </span>
-          <el-input
-            :key="passwordType"
-            ref="password"
-            v-model="loginForm.password"
-            :type="passwordType"
-            placeholder="密码"
-            name="password"
-            tabindex="2"
-            autocomplete="on"
-            @keyup.native="checkCapslock"
-            @blur="capsTooltip = false"
-            @keyup.enter.native="handleLogin"
-          />
-          <span class="show-pwd" @click="showPwd">
-            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-          </span>
-        </el-form-item>
-      </el-tooltip>
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <svg-icon icon-class="password" />
+        </span>
+        <el-input
+          ref="password"
+          v-model="loginForm.password"
+          placeholder="密码"
+          name="password"
+          type="password"
+          tabindex="2"
+          @keyup.enter.native="handleLogin"
+        />
+      </el-form-item>
 
       <el-button
         :loading="loading"
@@ -72,7 +61,7 @@ export default {
     // 用户名验证
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error("检查用户名"));
+        callback(new Error("请检查用户名"));
       } else {
         callback();
       }
@@ -80,26 +69,24 @@ export default {
     // 密码验证
     const validatePassword = (rule, value, callback) => {
       if (value.length < 5) {
-        callback(new Error("检查密码"));
+        callback(new Error("请检查密码"));
       } else {
         callback();
       }
     };
     return {
       loginForm: {
-        username: "admin",
-        password: "admin",
+        username: "",
+        password: "",
       },
       loginRules: {
         username: [
-          { required: true, trigger: "submit", validator: validateUsername },
+          { required: true, trigger: "change", validator: validateUsername },
         ],
         password: [
-          { required: true, trigger: "submit", validator: validatePassword },
+          { required: true, trigger: "change", validator: validatePassword },
         ],
       },
-      passwordType: "password",
-      capsTooltip: false,
       loading: false,
       redirect: undefined,
       otherQuery: {},
@@ -125,22 +112,10 @@ export default {
     }
   },
   methods: {
-    checkCapslock(e) {
-      const { key } = e;
-      this.capsTooltip = key && key.length === 1 && key >= "A" && key <= "Z";
-    },
-    showPwd() {
-      if (this.passwordType === "password") {
-        this.passwordType = "";
-      } else {
-        this.passwordType = "password";
-      }
-      this.$nextTick(() => {
-        this.$refs.password.focus();
-      });
-    },
     // 登录逻辑
     handleLogin() {
+      // this.$router.push({ path: "/admin" });
+      // return;
       this.$refs.loginForm.validate((valid) => {
         if (valid) {
           this.loading = true;
@@ -243,17 +218,6 @@ $light_gray: #eee;
     overflow: hidden;
   }
 
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
-  }
 
   .svg-container {
     padding: 6px 5px 6px 15px;
@@ -275,26 +239,6 @@ $light_gray: #eee;
     }
   }
 
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
 
-  .thirdparty-button {
-    position: absolute;
-    right: 0;
-    bottom: 6px;
-  }
-
-  @media only screen and (max-width: 470px) {
-    .thirdparty-button {
-      display: none;
-    }
-  }
 }
 </style>
